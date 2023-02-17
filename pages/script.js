24f98537a58705404b393f8c2669d105b65594ad
Debora Santos <debora_breslau1@icloud.com>
2023-02-18 08:42:26 +0900

    add: funcrtion heart and Save

--- pages/script.js
+++ pages/script.js
@@ -7,19 +7,39 @@ function closePopup() {
     popup.classList.remove("open-popup");
 };
 
+
+const saveBtn = document.getElementById("save-btn");
+const userName = document.getElementById("userName");
+const userText = document.getElementById("userText");
+const profileName = document.getElementById("profileName");
+const profileText = document.getElementById("profileText");
+
+function savePopup() {
+  const inputValueUserName = userName.value;
+  const inputValueUserText = userText.value;
+
+  profileName.textContent = inputValueUserName;
+  profileText.textContent = inputValueUserText;
+}
+saveBtn.addEventListener('submit', function(savePopup) {
+    saveForm.preventDefault(); 
+    saveForm(); 
+  });
+
+
+const heart = document.getElementById("likeImage");
+
+let toggle = true;
+
+heart.addEventListener('click', function toggleImage(){
+    toggle = !toggle;
+
+    if (toggle){
+        heart.src = '/images/like.png';
+    }else {
+        heart.src= 'images/blackheart.png';
+    }
+    
 });
 
-
-
-const heart = document.querySelectorAll("card__like_btn");
-
-heart.forEach(function(heart) {
-    heart.addEventListener('click', function() {
-      const image = heart.querySelector('img');
-      if (image.src.endsWith('like.png')) {
-        image.src = './images/blackheart.png';
-      } else {
-        image.src = './images/like.png';
-      }
-    });
-  });
+   
