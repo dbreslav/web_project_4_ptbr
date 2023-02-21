22d55805d01b7e8b07a6fd39a8996c143ee9e04c
Debora Santos <debora_breslau1@icloud.com>
2023-02-21 13:06:29 +0900

    add: BEM folders and import

--- pages/script.js
+++ pages/script.js
@@ -1,41 +1,25 @@
 let popup = document.getElementById("popup");
 
 function openPopup() {
-    popup.classList.add("open-popup");
+  popup.classList.add("open-popup");
 }
 function closePopup() {
-    popup.classList.remove("open-popup");
-};
-
-
+  popup.classList.remove("open-popup");
+}
 
 function savePopup() {
-    const usernameInput = document.getElementById('username');
-    const userTextInput = document.getElementById('usertext');
-    const profileName = document.getElementById('profileName');
-    const profileText = document.getElementById('profileText');
-  
-    profileName.textContent = usernameInput.value;
-    profileText.textContent = userTextInput.value;
-  
-    closePopup();
-  }
-  
+  const usernameInput = document.getElementById("username");
+  const userTextInput = document.getElementById("usertext");
+  const profileName = document.getElementById("profileName");
+  const profileText = document.getElementById("profileText");
 
+  profileName.textContent = usernameInput.value;
+  profileText.textContent = userTextInput.value;
 
+  closePopup();
+}
 
-  function toggleLikeButton(event) {
-    console.log(event);
-event.target.classList.toggle('card__like_btn-on');
-    
-      
-    }
-  
-  
-  
-  
-  
-
-
-
-   
+function toggleLikeButton(event) {
+  console.log(event);
+  event.target.classList.toggle("card__like_btn-on");
+}
