e2f8b1f1ccb429b642c2dc9c6ad41cfcdabc41dc
Debora Santos <debora_breslau1@icloud.com>
2023-03-25 21:04:54 +0900

    feat: JS code for creating 6 cards

--- pages/script.js
+++ pages/script.js
@@ -42,3 +42,74 @@ for (let likeButton of likeButtons) {
     event.target.classList.toggle("heart-on");
   });
 }
+
+//function add cards
+
+const cardsContainer = document.getElementsByClassName("cards")[0];
+
+const initialCards = [
+  {
+    name: "Vale de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
+    alt: "Vista do Vale de Yosemite",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
+    alt: "Vista do Lago Louise no Parque Nacional de Banff",
+  },
+  {
+    name: "Montanhas Carecas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
+    alt: "Vista das Montanhas Carecas",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
+    alt: "Vista da cordilheira Latemar na Itália",
+  },
+  {
+    name: "Parque Nacional da Vanoise ",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
+    alt: "Vista do Parque Nacional da Vanoise na França",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
+    alt: "Vista do Lago di Braies na Itália",
+  },
+];
+
+function addCards() {
+  initialCards.forEach((card) => {
+    const cardElement = document.createElement("div");
+    cardElement.classList.add("card");
+
+    const imageElement = document.createElement("img");
+    imageElement.classList.add("card__image");
+    imageElement.src = card.link;
+    imageElement.alt = card.alt;
+
+    const bottomBar = document.createElement("div");
+    bottomBar.classList.add("card__bottom-bar");
+
+    const cardTitle = document.createElement("p");
+    cardTitle.classList.add("card__title");
+    cardTitle.src = card.name;
+
+    const likeBtn = document.createElement("button");
+    likeBtn.classList.add("heart-off");
+
+    const trashBtn = document.createElement("button");
+    trashBtn.classList.add("trash-btn");
+
+    bottomBar.appendChild(cardTitle);
+    bottomBar.appendChild(likeBtn);
+    cardElement.appendChild(imageElement);
+    cardElement.appendChild(bottomBar);
+    cardElement.appendChild(trashBtn);
+    cardsContainer.appendChild(cardElement);
+  });
+}
+
+addCards();
